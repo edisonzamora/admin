eea2dc0a73f3ebb846209e89f534900c2a34d710
Edison Zamora <37243420+edisonzamora@users.noreply.github.com>
2022-06-08 17:08:00 +0200

    enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'listaUsuarios',component:UsuariosComponent},
+{path:'añadirUsuarios',component:AddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
