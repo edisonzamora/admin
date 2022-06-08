4e263fb9ed747918f2e2e5fd5b9d7fc3e00ea3a4
Edison Zamora <37243420+edisonzamora@users.noreply.github.com>
2022-06-08 17:13:26 +0200

    actualizando app.module
    registramos los componentes y los servicios. creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { AddComponent } from './add/add.component';
+
+import {UsuariosServiceService} from '../app/Service/usuarios-service.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UsuariosServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
